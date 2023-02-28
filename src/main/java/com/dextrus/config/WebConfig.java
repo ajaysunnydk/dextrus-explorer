12095120c48c56783bcc65588d1b73b9098c16e6
ajaysunnydk <ajaysunnydk167@gmail.com>
2023-02-28 11:39:26 +0530

    Update WebConfig.java

--- src/main/java/com/dextrus/config/WebConfig.java
+++ src/main/java/com/dextrus/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOriginPatterns("http://localhost:3000")
+        .allowedOriginPatterns("http://localhost:3000","http://192.168.0.150:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
